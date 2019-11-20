3b22bcd7e7b8a4be6aff7a9d67ad23b494a1983c
AmruthjithSunil <amruthjithsunil@gmail.com>
2019-11-20 12:21:32 +0530

    Added Some Comments

--- interpreter.c
+++ interpreter.c
@@ -1,20 +1,32 @@
 #include<stdio.h>
 
-int a[100];
-char s[100];
+int a[100];                         //Memory of the program
+int MemoryIndex = 0;                //Index of the Memory
+char s[100];                        //to store the input string
 
 void main(){
     int StringValid();
+    void Compile();
+    printf("BrainFuck 0.0.1\nType \"exit\" or \"quit\" to terminate.\nType \"help\", \"copyright\", \"credits\" or \"license\" for more information.\n");
     do {
         printf("fuck ~ ");
         scanf("%s",s);
+        Compile();
     }while(StringValid());
 }
 
+//to check the input string is valid or not
 int StringValid(){
     for(int i = 0; s[i]!='\0'; i++){
         if(s[i]!='[' && s[i]!=']' && s[i]!='<' && s[i]!='>' && s[i]!='.' && s[i]!=',' && s[i]!='+' && s[i]!='-')
             return 0;
     }
     return 1;
+}
+
+//to compile the input string
+void Compile(){
+    for(int i = 0; s[i]!='\0'; i++){
+
+    }
 }
